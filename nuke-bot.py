a21e8aa38e67e2472138c29b541e2377f9affd76
Render <165334302+RenderBolt96@users.noreply.github.com>
2024-07-12 13:45:31 -0500

    Update nuke-bot.py

--- nuke-bot.py
+++ nuke-bot.py
@@ -21,7 +21,7 @@ baner = f'''
 {r}|  \| | | | | |/ / _ {m}\  _ \ / _ \| __|
 {r}| |\  | |_| |   <  __{m}/ |_) | (_) | |_ 
 {r}|_| \_|\__,_|_|\_\___{m}|____/ \___/ \__|
-{y}Made by: {g}https://github.com/Sigma-cc'''
+{y}Made by: {g}https://github.com/RenderBolt96/Nuke-Bot.git'''
 
 
 
